060fa184aa8fda3a466d6ec9b4d9b5bb98cefba3
André Botelho <andrebotelho@live.com>
2018-06-18 21:01:35 -0300

    Segundo

---
+++ PSA-TPF-Parking/Negocio/Cancela.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking.Negocio
+{
+    public class Cancela
+    {
+        public Ticket gerarTicket()
+        {
+        return null;
+        }
+        public bool ConsultaTicket(Ticket ticket)
+        {
+        return true;
+        }
+        public void registraSaida(Ticket ticket)
+        {
+
+        }
+    }
+}
---
+++ PSA-TPF-Parking/Negocio/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking.Negocio
+{
+    public class Ticket
+    {
+        public int codigo { get; set; }
+        public DateTime dataHoraEmissao { get; set; }
+    }
+}

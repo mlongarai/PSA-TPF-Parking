8b0e52f0aa945a4b252a95c97b7dc479aafc2fd0
MLongarai <mlongarai@users.noreply.github.com>
2018-06-21 19:47:35 -0300

    Add files via upload
    LocalDb

--- Parking/Startup.cs
+++ Parking/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Parking.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Parking
 {
@@ -21,6 +23,9 @@ namespace Parking
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddDbContext<ParkingContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Parking")));
             services.AddMvc();
         }
 

9556f42a1ca4514e08019447217468b7752808e1
MLongarai <mlongarai@users.noreply.github.com>
2018-06-21 19:45:41 -0300

    Add files via upload
    LocalDb

---
+++ Parking/Models/ParkingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking.Models
+{
+    public class ParkingContext : DbContext
+    {
+        public ParkingContext(DbContextOptions<ParkingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ParkingContext> Parking { get; set; }
+    }
+}

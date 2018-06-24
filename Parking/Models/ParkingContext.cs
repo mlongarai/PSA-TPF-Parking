9154301ba679a1853cdd30a1d59c02b60ada7e99
Douglas Amengual Severo <douglas.amengual@gmail.com>
2018-06-23 22:41:12 -0300

    commit migration e data base

--- Parking/Models/ParkingContext.cs
+++ Parking/Models/ParkingContext.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Parking.business;
 
 namespace Parking.Models
 {
@@ -13,6 +16,16 @@ namespace Parking.Models
         {
         }
 
-        public DbSet<ParkingContext> Parking { get; set; }
+        public DbSet<Ticket> Parking { get; set; }
+
+        public static readonly LoggerFactory FabricaLogger = new LoggerFactory(new[] { new
+ConsoleLoggerProvider((_, __) => true, true) });
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseLoggerFactory(FabricaLogger)
+
+           .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Parking;Trusted_Connection=True;");
+        }
     }
 }
